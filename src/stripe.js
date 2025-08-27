3dea02e86eb6365a83569e06599bba17ff2f43e1
pullum327 <91268908+pullum327@users.noreply.github.com>
2025-08-27 20:34:17 +0800

    Delete src directory

--- src/stripe.js
+++
@@ -1,6 +0,0 @@
-import { loadStripe } from '@stripe/stripe-js';
-import { STRIPE_CONFIG } from './config';
-
-const stripePromise = loadStripe(STRIPE_CONFIG.publishableKey);
-
-export default stripePromise;
